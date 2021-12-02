351c0bde2d9e347396730535726db9129a604143
Nick18899 <51851229+Nick18899@users.noreply.github.com>
2021-12-02 10:59:26 +0300

    Update task-3.py

--- task-3.py
+++ task-3.py
@@ -5,7 +5,7 @@ for i in f:
 
 true_data = []
 for i in data:
-    if (i % 100) / 10 <= 4 and 3 <= (i % 1000) / 100 <= 7:
+    if (i % 100) // 10 <= 4 and 3 <= (i % 1000) // 100 <= 7:
         true_data.append(i)
 # print(true_data)
 print(sorted(true_data))
